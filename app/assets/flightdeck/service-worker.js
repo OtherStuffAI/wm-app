516c078124d98dcff427d1a6daf0a18aaa786ef8
Pete Winn <petewinn@gmail.com>
2026-08-14 13:53:55 +0800

    Bundle paint-first Flight Deck navigation

--- app/assets/flightdeck/service-worker.js
+++ app/assets/flightdeck/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260814-0450-16-1796";
+const BUILD_ID = "20260814-0551-17-1797";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
