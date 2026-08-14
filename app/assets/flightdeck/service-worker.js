22d644aa4f4ff4281e84e22905b7a784abe41886
Pete Winn <petewinn@gmail.com>
2026-08-14 14:01:17 +0800

    Bundle corrected navigation timing build

--- app/assets/flightdeck/service-worker.js
+++ app/assets/flightdeck/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260814-0551-17-1797";
+const BUILD_ID = "20260814-0600-18-1798";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
