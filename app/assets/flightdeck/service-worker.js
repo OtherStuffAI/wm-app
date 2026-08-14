d48353a32e8afd53746ffbfd8c5f9e137e9ca4c4
humansinstitute <petewinn@gmail.com>
2026-08-14 11:30:36 +0800

    local flight deck tower connect

--- app/assets/flightdeck/service-worker.js
+++ app/assets/flightdeck/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260814-0258-8-1788";
+const BUILD_ID = "20260814-0326-10-1790";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
