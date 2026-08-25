9433a5ca818772703410843fa59531ebd8f40664
Pete Winn <petewinn@gmail.com>
2026-08-25 19:42:22 +0800

    Repackage Flight Deck reconnect signing fix

--- app/assets/flightdeck/service-worker.js
+++ app/assets/flightdeck/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260825-0919-3-1818";
+const BUILD_ID = "20260825-1141-7-1821";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
