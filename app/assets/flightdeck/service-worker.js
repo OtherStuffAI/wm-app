65d49179c4ce3d69210f9c779203adc89bc7dd29
Pete Winn <petewinn@gmail.com>
2026-09-05 18:48:44 +0800

    fix(flightdeck): bundle Inbox history build 1882

--- app/assets/flightdeck/service-worker.js
+++ app/assets/flightdeck/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260905-0845-7-1878";
+const BUILD_ID = "20260905-1046-11-1882";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
