3ad64f5edd96fc50d9feda71fb694fc1b6f4cb33
Pete Winn <petewinn@gmail.com>
2026-09-05 16:51:21 +0800

    fix(flightdeck): bundle bounded activity build 1878

--- app/assets/flightdeck/service-worker.js
+++ app/assets/flightdeck/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260905-0819-6-1877";
+const BUILD_ID = "20260905-0845-7-1878";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
