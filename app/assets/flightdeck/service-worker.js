3fd68f8b8da6a5f353a5d53937a43c969e21efab
Pete Winn <petewinn@gmail.com>
2026-09-08 15:25:24 +0800

    build(ios): bundle compact mobile inbox build 1911

--- app/assets/flightdeck/service-worker.js
+++ app/assets/flightdeck/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260906-0507-1-1884";
+const BUILD_ID = "wmapp-db2cde3b7a59-1911";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
