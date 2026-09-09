a4915ff3e08d471f12b9cf2855d6bb7f5ce13cfd
Pete Winn <petewinn@gmail.com>
2026-09-09 19:19:09 +0800

    build(flightdeck): bundle exact workspace FIPS pairing release

--- app/assets/flightdeck/service-worker.js
+++ app/assets/flightdeck/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260909-1111-6-1919";
+const BUILD_ID = "wmapp-d07cc8139ee4-1919";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
